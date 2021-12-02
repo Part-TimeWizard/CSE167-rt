df18117972438867700ed9bd80744db84c783dac
Part-TimeWizard <nikmitra@gmail.com>
2021-12-01 23:13:40 -0800

    Primitive header file

--- include/Primitive.h
+++ include/Primitive.h
@@ -1,7 +1,7 @@
 /**
  *  Primitive.h
  *  
- * 
+ *  Parent class for Sphere and Triangle, contains material data and virtual class to check for ray intersection.
  */
 #include <glm/glm.hpp>
 #pragma once
@@ -9,14 +9,21 @@
 class Primitive {
 
     private:
-        glm::vec4 diffuse;
-        glm::vec4 specular;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
         float shininess;
-        glm::vec4 emission;
+        glm::vec3 emission;
 
     public:
         virtual void checkHit(glm::vec3);
 
         void setDiffuse(glm::vec3 d){ diffuse = d; };
         void setSpecular(glm::vec3 s){ specular = s; };
+        void setShininess(float s){ shininess = s; };
+        void setEmission(glm::vec3 e){ emission = e; };
+
+        glm::vec3 getDiffuse(){ return diffuse; };
+        glm::vec3 getSpecular(){ return specular; };
+        float getShininess(){ return shininess; };
+        glm::vec3 getEmission(){ return emission; };
 };
