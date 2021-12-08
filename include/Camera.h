5143963cbff9aa6e6516f1bd68c846d8a959fff3
James Yuntae Lee <y2lee@ucsd.edu>
2021-12-08 03:33:33 -0800

    More updates to Camera
    Created separate U and V vectors for camera and projection

--- include/Camera.h
+++ include/Camera.h
@@ -11,15 +11,19 @@ public:
 	glm::vec3 eye;		// position of camera
 	glm::vec3 target;	// position camera is looking at
 	glm::vec3 up;		// up vector 
-	float fovy;			// input fov 
-	float fovx;			// fov dependent on image size
+	float aspr; 		// image aspect ratio 
+	float fovy;			// vertical fov 
+	float fovx;			// horizontal fov 
 	int h; 				// image size 
 	int w; 
 
-	glm::vec3 p_a; 		// alignment vector of projection (W vector) 
+	glm::vec3 c_w; 		// W vector of camera
+	glm::vec3 c_u;		// U vector of camera 
+	glm::vec3 c_v; 		// V vector of camera 
+
+	glm::vec3 p_c;		// center position of projection 
 	glm::vec3 p_u;		// U vector of projection 
 	glm::vec3 p_v; 		// V vector of projection 
-	glm::vec3 p_c;		// center position of projection 
 
 	Camera(); 
 	Camera(const glm::vec3 position, const glm::vec3 direction, const glm::vec3 normal, const float fov, const int height, const int width); 
