dcc9fd1f35946c3f3cdc153a70a90a01caa79e23
James Yuntae Lee <y2lee@ucsd.edu>
2021-12-08 22:39:30 -0800

    Updated Sphere.cpp
    Sphere has been made

--- src/models/Sphere.cpp
+++ src/models/Sphere.cpp
@@ -5,48 +5,50 @@
  */
 #include "Sphere.h"
 
-Sphere::Sphere(glm::vec3 center, float radius, glm::mat4 t, glm::vec3 a, glm::vec3 sp, glm::vec3 d, glm::vec3 e, float sh) {
-    c = center; 
-    r = radius; 
-    transform = t; 
-}
-
-void Sphere::transform(glm::vec3) {
-    return;
-}
-
 glm::vec4 Sphere::checkHit(Ray ray) {
     glm::vec3 p0 = ray.ori;
     glm::vec3 d = glm::normalize(ray.dir);
 
     // Transform ray to sphere's space
     glm::vec4 pTransformed0 = glm::inverse(transform) * glm::vec4(p0,1.0f);
-    glm::vec4 dTransformed = glm::inverse(glm::mat3(transform)) * d;
+    glm::vec3 dTransformed = glm::inverse(glm::mat3(transform)) * d;
     
-    glm::vec3 pmc = p0 - c;                 // p - c 
-    float dpc = glm::dot((-1.0 * d), pmc);  // dot product of -d and p0-c 
-    float t;                                // t used for intersection 
+    // (o + td - c)^2 - R^2 = 0
+    // at^2 + bt + c = 0 where 
+    // a = d dot d 
+    // b = 2 * (p0 - c) dot d 
+    // c = (p0 - c) dot (p0 - c) - R^2 
+    float a = glm::dot(d, d); 
+    float b = 2 * glm::dot((p0 - c), d); 
+    float c = glm::dot((p0 - c), (p0 - c)) - std::powf(r, 2.0f); 
 
-    // inside sqrt test 
-    float sqrt = std::powf(glm::dot(d, pmc), 2.0f) - 
-    std::powf(std::abs(pmc), 2) + std::powf(r, 2); 
+    // Quadratic formula for t 
+    float t1 = (-1.0f * b + std::sqrt(std::pow(b, 2) - 4 * a * c)) / (2 * a); 
+    float t2 = (-1.0f * b - std::sqrt(std::pow(b, 2) - 4 * a * c)) / (2 * a); 
 
-    // intersection test 
-    if (sqrt <= 0) { return glm::vec4(q, 0); }
-    else { 
-        float t1 = dpc + std::sqrtf(sqrt); 
-        float t2 = dpc - std::sqrtf(sqrt); 
-        if (t1 < t2 && t1 > 0) { t = t1; }  // find smaller and positive t 
-        else if (t2 > 0) { t = t2; }
+    if (t1 > t2) {
+        // swap variables if first is bigger 
+        // we want smaller positive t 
+        float temp = t1; 
+        t1 = t2; 
+        t2 = temp; 
     }
+    // no intersect cases 
+    if (t2 < 0) { return glm::vec4(0.0f); } 
+    if (t1 < 0) { return glm::vec4(0.0f); }
 
-    glm::vec3 q = p0 + t * d; 
+    // position of intersection 
+    glm::vec3 q = p0 + t1 * d; 
     glm::vec3 n = glm::normalize(q - c); 
 
     // Transform back
-    glm::vec4 qTransformed = transform * glm::vec4(q,1);
-    glm::vec3 nTransformed = glm::transpose(glm::mat3(transform))*n;
+    glm::vec4 qTransformed = transform * glm::vec4(q[0], q[1], q[2], 1);
+    glm::vec3 nTransformed = glm::transpose(glm::mat3(transform)) * n;
     nTransformed = glm::normalize(nTransformed);
 
-    t = std::abs(qTransformed - nTransformed); 
+    glm::vec3 newQ(qTransformed[0], qTransformed[1], qTransformed[2]); 
+
+    glm::vec3 newT = glm::abs(newQ - p0); 
+    glm::vec4 finT(newT[0], newT[1], newT[2], 1.0f); 
+    return finT; 
 }
